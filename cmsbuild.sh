13068973111023e2a3d93f49c2bb78ed4afe0a0e
Adrian Sutherland <adrian@sutherlandonline.org>
2021-04-13 17:36:09 +0100

    Include MAINTC Build Tools and save gccbrx.cckd in release assets

--- cmsbuild.sh
+++ cmsbuild.sh
@@ -17,7 +17,8 @@ herccontrol "/purge rdr" -w "^Ready;"
 herccontrol "/ACCESS 394 A" -w "^Ready;"
 herccontrol "/ERASE * * A1" -w "^Ready;"
 
-# Read/Send ARCHIVE  YATA
+# Send Source Code - including tools
+cp tools/* .
 yata -c
 herccontrol -m >tmp; read mark <tmp; rm tmp
 echo "USERID  MAINTC\n:READ  ARCHIVE  YATA    " > tmp
@@ -32,7 +33,7 @@ herccontrol "/yata -x -f READER -d a" -w "^Ready;" -t 120
 herccontrol "/COPYFILE * MACRO    A (RECFM F LRECL 80" -w "^Ready"
 herccontrol "/COPYFILE * COPY     A (RECFM F LRECL 80" -w "^Ready"
 herccontrol "/COPYFILE * ASSEMBLE A (RECFM F LRECL 80" -w "^Ready"
-herccontrol "/COPYFILE * EXEC     A (RECFM F" -w "^Ready"
+#herccontrol "/COPYFILE * EXEC     A (RECFM F" -w "^Ready"
 
 # Make source tape and vmarc
 herccontrol "/cp disc" -w "^VM/370 Online"
@@ -52,11 +53,35 @@ herccontrol "/vmarc pack * * a (pun notrace" -w "^Ready;"
 herccontrol "devinit 00d dummy" -w "^HHCPN098I"
 truncate -s-80 gcclibsrc.vmarc
 
+# Put tools in the T drive
+herccontrol "/COPYFILE GCCASM EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCASM EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCBUILD EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCBUILD EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCCOMP EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCCOMP EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCGEN EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCGEN EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCGENM EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCGENM EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCSRCH EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCSRCH EXEC A" -w "^Ready"
+
 herccontrol "/ipl cms" -w "^CMS"
 herccontrol "/" -w "^Ready;"
 
 herccontrol "/GCCBUILD" -w "^Ready;" -t 240
 herccontrol "/GCCGENM" -w "^Ready;"
+
+herccontrol "/ipl cms" -w "^CMS"
+herccontrol "/" -w "^Ready;"
+
+herccontrol "/GCCSRCH" -w "^Ready;"
 herccontrol "/GCCGEN" -w "^Ready;"
 
 herccontrol "/ipl cms" -w "^CMS"
@@ -72,10 +97,15 @@ herccontrol "devinit 00d io/gcclibbin.vmarc" -w "^HHCPN098I"
 herccontrol "/cp disc" -w "^VM/370 Online"
 herccontrol "/logon maintc maintc" -w "RECONNECTED AT"
 herccontrol "/begin"
-herccontrol "/tape dump gcclib * a (noprint" -w "^Ready;"
-herccontrol "/tape dump gccres * a (noprint" -w "^Ready;"
+herccontrol "/access 194 e" -w "^Ready;"
+herccontrol "/copyfile gcclib * a = = e" -w "^Ready;"
+herccontrol "/copyfile gccres * a = = e" -w "^Ready;"
+#herccontrol "/tape dump gcclib * a (noprint" -w "^Ready;"
+#herccontrol "/tape dump gccres * a (noprint" -w "^Ready;"
+herccontrol "/tape dump * * e (noprint" -w "^Ready;"
 herccontrol "/detach 181" -w "^Ready;"
-herccontrol "/vmarc pack gcc* * a (pun notrace" -w "^Ready;"
+#herccontrol "/vmarc pack gcc* * a (pun notrace" -w "^Ready;"
+herccontrol "/vmarc pack * * e (pun notrace" -w "^Ready;"
 
 # Close and remove extra record from VMARC file
 herccontrol "devinit 00d dummy" -w "^HHCPN098I"
--- cp2vm370.sh
+++ cp2vm370.sh
@@ -14,8 +14,19 @@ cat tmp.txt >> archive.yata
 docker cp archive.yata vm370:/opt/hercules/vm370/io
 rm archive.yata
 rm tmp.txt
-
 docker exec vm370 bash -c "cd /opt/hercules/vm370/io && yata -x"
 docker exec vm370 bash -c "rm /opt/hercules/vm370/io/archive.yata"
+
+# Tools
+yata -c -d tools -f tmp.txt
+echo "USERID  CMSUSER\n:READ  ARCHIVE  YATA    " > archive.yata
+cat tmp.txt >> archive.yata
+docker exec vm370 bash -c "mkdir /opt/hercules/vm370/io/tools"
+docker cp archive.yata vm370:/opt/hercules/vm370/io/tools
+rm archive.yata
+rm tmp.txt
+docker exec vm370 bash -c "cd /opt/hercules/vm370/io/tools && yata -x"
+docker exec vm370 bash -c "rm /opt/hercules/vm370/io/tools/archive.yata"
+
 docker cp cmsbuild.sh vm370:/opt/hercules/vm370/io
 docker exec vm370 bash -c "cd /opt/hercules/vm370/io && ./cmsbuild.sh"
