8babe4674df8206e8666af3a991b8c0a166fe32a
Adrian Sutherland <adrian@sutherlandonline.org>
2021-04-14 12:23:45 +0100

    Feature/f0054 (#56)
    * Initial Check-in for version 1.0.0 build on VM/370 CE
    
    * Include MAINTC Build Tools and save gccbrx.cckd in release assets
    
    * Fix disk location
    
    * Detach gccbrx.cckd

--- cmsbuild.sh
+++ cmsbuild.sh
@@ -5,80 +5,111 @@
 set -e
 
 # IPL
-herccontrol -v
 herccontrol "ipl 141" -w "USER DSC LOGOFF AS AUTOLOG1"
 herccontrol "/cp start c" -w "RDR"
 herccontrol "/cp start d class a" -w "PUN"
 
-# LOGON CMSUSER
+# LOGON MAINTC
 herccontrol "/cp disc" -w "^VM/370 Online"
-herccontrol "/logon cmsuser cmsuser" -w "^CMS VERSION"
+herccontrol "/logon maintc maintc" -w "^CMS"
 herccontrol "/" -w "^Ready;"
+herccontrol "/purge rdr" -w "^Ready;"
+herccontrol "/ACCESS 394 A" -w "^Ready;"
+herccontrol "/ERASE * * A1" -w "^Ready;"
 
-# Read/Send TATA TXT
+# Send Source Code - including tools
+cp tools/* .
 yata -c
 herccontrol -m >tmp; read mark <tmp; rm tmp
-echo "USERID  CMSUSER\n:READ  YATA     TXT     " > tmp
-cat yata.txt >> tmp
+echo "USERID  MAINTC\n:READ  ARCHIVE  YATA    " > tmp
+cat archive.yata >> tmp
 netcat -q 0 localhost 3505 < tmp
 rm tmp
 herccontrol -w "HHCRD012I" -f $mark
 herccontrol "/" -w "RDR FILE"
 
 # Prepare Source
-herccontrol "/yata -x -f READER -d f" -w "^Ready;" -t 120
-herccontrol "/fixrecfm" -w "^Ready;"
+herccontrol "/yata -x -f READER -d a" -w "^Ready;" -t 120
+herccontrol "/COPYFILE * MACRO    A (RECFM F LRECL 80" -w "^Ready"
+herccontrol "/COPYFILE * COPY     A (RECFM F LRECL 80" -w "^Ready"
+herccontrol "/COPYFILE * ASSEMBLE A (RECFM F LRECL 80" -w "^Ready"
+#herccontrol "/COPYFILE * EXEC     A (RECFM F" -w "^Ready"
 
 # Make source tape and vmarc
 herccontrol "/cp disc" -w "^VM/370 Online"
 herccontrol "/logon operator operator" -w "RECONNECTED AT"
 hetinit -n -d gcclibsrc.aws
 herccontrol "devinit 480 io/gcclibsrc.aws" -w "^HHCPN098I"
-herccontrol "/attach 480 to cmsuser as 181" -w "TAPE 480 ATTACH TO CMSUSER"
+herccontrol "/attach 480 to maintc as 181" -w "TAPE 480 ATTACH"
 herccontrol "devinit 00d io/gcclibsrc.vmarc" -w "^HHCPN098I"
 herccontrol "/cp disc" -w "^VM/370 Online"
-herccontrol "/logon cmsuser cmsuser" -w "RECONNECTED AT"
+herccontrol "/logon maintc maintc" -w "RECONNECTED AT"
 herccontrol "/begin"
-herccontrol "/tape dump * * f (noprint" -w "^Ready;"
+herccontrol "/tape dump * * a (noprint" -w "^Ready;"
 herccontrol "/detach 181" -w "^Ready;"
-herccontrol "/vmarc pack * * f (pun notrace" -w "^Ready;"
+herccontrol "/vmarc pack * * a (pun notrace" -w "^Ready;"
 
-# Build
-herccontrol "/build" -w "^Ready;" -t 240
-herccontrol "/rename * * e = = e2" -w "^Ready;"
-herccontrol "/cleanup" -w "^Ready;"
+# Close and remove extra record from VMARC file
+herccontrol "devinit 00d dummy" -w "^HHCPN098I"
+truncate -s-80 gcclibsrc.vmarc
 
-# Sanity test
-herccontrol "/copy * * e = = a" -w "^Ready;"
-herccontrol "/ipl cms" -w "^CMS VERSION"
+# Put tools in the T drive
+herccontrol "/COPYFILE GCCASM EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCASM EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCBUILD EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCBUILD EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCCOMP EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCCOMP EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCGEN EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCGEN EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCGENM EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCGENM EXEC A" -w "^Ready"
+
+herccontrol "/COPYFILE GCCSRCH EXEC A = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE GCCSRCH EXEC A" -w "^Ready"
+
+herccontrol "/ipl cms" -w "^CMS"
 herccontrol "/" -w "^Ready;"
-herccontrol "/mktest" -w "^Ready;" -t 240
+
+herccontrol "/GCCBUILD" -w "^Ready;" -t 240
+herccontrol "/GCCGENM" -w "^Ready;"
+
+herccontrol "/ipl cms" -w "^CMS"
+herccontrol "/" -w "^Ready;"
+
+herccontrol "/GCCSRCH" -w "^Ready;"
+herccontrol "/GCCGEN" -w "^Ready;"
+
+herccontrol "/ipl cms" -w "^CMS"
+herccontrol "/" -w "^Ready;"
 
 # Make binary tape and vmarc
 herccontrol "/cp disc" -w "^VM/370 Online"
 herccontrol "/logon operator operator" -w "RECONNECTED AT"
 hetinit -n -d gcclibbin.aws
 herccontrol "devinit 480 io/gcclibbin.aws" -w "^HHCPN098I"
-herccontrol "/attach 480 to cmsuser as 181" -w "TAPE 480 ATTACH TO CMSUSER"
+herccontrol "/attach 480 to maintc as 181" -w "TAPE 480 ATTACH"
 herccontrol "devinit 00d io/gcclibbin.vmarc" -w "^HHCPN098I"
 herccontrol "/cp disc" -w "^VM/370 Online"
-herccontrol "/logon cmsuser cmsuser" -w "RECONNECTED AT"
+herccontrol "/logon maintc maintc" -w "RECONNECTED AT"
 herccontrol "/begin"
+herccontrol "/access 194 e" -w "^Ready;"
+herccontrol "/copyfile gcclib * a = = e" -w "^Ready;"
+herccontrol "/copyfile gccres * a = = e" -w "^Ready;"
+#herccontrol "/tape dump gcclib * a (noprint" -w "^Ready;"
+#herccontrol "/tape dump gccres * a (noprint" -w "^Ready;"
 herccontrol "/tape dump * * e (noprint" -w "^Ready;"
 herccontrol "/detach 181" -w "^Ready;"
+#herccontrol "/vmarc pack gcc* * a (pun notrace" -w "^Ready;"
 herccontrol "/vmarc pack * * e (pun notrace" -w "^Ready;"
 
-# Clean Up
-herccontrol "/erase gcclib * a" -w "^Ready;"
-herccontrol "/erase gccres * a" -w "^Ready;"
-herccontrol "/erase * h a" -w "^Ready;"
-herccontrol "/erase test * a" -w "^Ready;"
-herccontrol "/erase mktest exec a" -w "^Ready;"
-herccontrol "/erase sysprof exec a" -w "^Ready;"
-herccontrol "/cleane" -w "^Ready;"
-herccontrol "/copy cleanf exec f = = a" -w "^Ready;"
-herccontrol "/cleanf" -w "^Ready;"
-herccontrol "/erase cleanf exec a" -w "^Ready;"
+# Close and remove extra record from VMARC file
+herccontrol "devinit 00d dummy" -w "^HHCPN098I"
+truncate -s-80 gcclibbin.vmarc
 
 # LOGOFF
 herccontrol "/logoff" -w "^VM/370 Online"
@@ -86,7 +117,4 @@ herccontrol "/logoff" -w "^VM/370 Online"
 # SHUTDOWN
 herccontrol "/logon operator operator" -w "RECONNECTED AT"
 herccontrol "/shutdown" -w "^HHCCP011I"
-
-# Remove extra record from VMARC files
-truncate -s-80 gcclibsrc.vmarc
-truncate -s-80 gcclibbin.vmarc
+herccontrol "detach 09F0"
--- cp2vm370.sh
+++ cp2vm370.sh
@@ -7,19 +7,26 @@ export MSYS2_ARG_CONV_EXCL="vm370:;/opt"
 docker kill vm370
 docker pull adriansutherland/vm370:builder
 docker run --rm -d -p 3270:3270 -p 8038:8038 -p 3505:3505 --name vm370 adriansutherland/vm370:builder
-# docker run --rm -d -p 3270:3270 -p 8038:8038 -p 3505:3505 --name vm370 adriansutherland/vm370local:latest
 
 yata -c -f tmp.txt
-echo "USERID  CMSUSER\n:READ  YATA     TXT     " > yata.txt
-cat tmp.txt >> yata.txt
-docker cp yata.txt vm370:/opt/hercules/vm370/io
-rm yata.txt
+echo "USERID  CMSUSER\n:READ  ARCHIVE  YATA    " > archive.yata
+cat tmp.txt >> archive.yata
+docker cp archive.yata vm370:/opt/hercules/vm370/io
+rm archive.yata
 rm tmp.txt
-
 docker exec vm370 bash -c "cd /opt/hercules/vm370/io && yata -x"
-docker exec vm370 bash -c "rm /opt/hercules/vm370/io/yata.txt"
-docker cp cmsbuild.sh vm370:/opt/hercules/vm370/io
-docker cp cmsinstall.sh vm370:/opt/hercules/vm370/io
+docker exec vm370 bash -c "rm /opt/hercules/vm370/io/archive.yata"
 
+# Tools
+yata -c -d tools -f tmp.txt
+echo "USERID  CMSUSER\n:READ  ARCHIVE  YATA    " > archive.yata
+cat tmp.txt >> archive.yata
+docker exec vm370 bash -c "mkdir /opt/hercules/vm370/io/tools"
+docker cp archive.yata vm370:/opt/hercules/vm370/io/tools
+rm archive.yata
+rm tmp.txt
+docker exec vm370 bash -c "cd /opt/hercules/vm370/io/tools && yata -x"
+docker exec vm370 bash -c "rm /opt/hercules/vm370/io/tools/archive.yata"
+
+docker cp cmsbuild.sh vm370:/opt/hercules/vm370/io
 docker exec vm370 bash -c "cd /opt/hercules/vm370/io && ./cmsbuild.sh"
-docker exec vm370 bash -c "cd /opt/hercules/vm370/io && ./cmsinstall.sh"
